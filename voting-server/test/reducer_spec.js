e2dc30dbac81c3740c84130401bc84fe50e5f10c
mkoltsov <mikhail.koltsov@grandparade.co.uk>
2016-05-01 17:08:11 +0200

    first exercise solved

--- voting-server/test/reducer_spec.js
+++ voting-server/test/reducer_spec.js
@@ -49,6 +49,26 @@ describe('reducer', () => {
     }));
   });
 
+  it('prevents VOTE from being made if it is not included in the current pair', () => {
+    const initialState = fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later']
+      },
+      entries: []
+    });
+    const action = {type: 'VOTE', entry: 'Trainspotting1'};
+
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later']
+      },
+      entries: []
+    }));
+  });
+
+
    it('has an initial state', () => {
     const action = {type: 'SET_ENTRIES', entries: ['Trainspotting']};
     const nextState = reducer(undefined, action);
